666565cdcfb5974c453b88041ddf0136ed33b3bb
Rail Sabirov <rails@fivestarlhcsa.com>
2024-06-24 08:50:50 -0400

    Verify email: migration

---
+++ database/migrations/2024_06_24_123744_create_verification_codes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('verification_codes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('code');
+            $table->timestamps();
+
+            $table->foreign('user_id') // связь изспользуя текущую переменную user_id
+                ->references('id') // сопоставляем с полем id в таблице users
+                ->on('users') // таблица users
+                ->onDelete('cascade'); // при удалении пользователя из табилцы users, удаляются все его коды верификации в текущей таблице
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('verification_codes');
+    }
+};
